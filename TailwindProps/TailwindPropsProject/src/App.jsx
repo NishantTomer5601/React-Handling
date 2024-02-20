72b7cd2fa275c1d7d2da558069205aa941beca19
Nishant Tomer <nishanttomer5601@gmail.com>
2024-02-20 16:47:05 +0530

    adding UI Components

--- TailwindProps/TailwindPropsProject/src/App.jsx
+++ TailwindProps/TailwindPropsProject/src/App.jsx
@@ -7,14 +7,15 @@ import Card from './component/Card'
 function App() {
   
   let myObj={ 
-    name:"Nishant"
+    LinkTo:"See Profile",
+    DispName:"Nishant Tomer"
   }
 
   return (
     <>
-      <h1 className='bg-green-400'>Tailwind Quick Guide</h1>
+      <h1 className='bg-gray-400'>Tailwind Quick Guide</h1>
       <br/>
-      <Card Profile={myObj.name}/>  
+      <Card Profile={myObj.LinkTo} userName={myObj.DispName}/>  
     </>
   )
 }
